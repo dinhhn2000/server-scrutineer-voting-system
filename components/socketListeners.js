f238977e6366261eb0b05cdda9f277aefdd23fee
CaoThanhHai <caothanhhai430@gmail.com>
2020-06-21 19:36:52 +0700

    update socket

--- components/socketListeners.js
+++ components/socketListeners.js
@@ -1,36 +1,63 @@
 const BlockChain = require("../utils/blockchain");
-const Vote = require("./votes/models");
 
 const room = '_room';
 var sockets = [];
-var currentChain = [];
+// var currentChain = [];
 const socketListeners = (io) => {
   io.on('connection', socket => {
+    console.log('id socket join ' + socket.id);
     sockets.push(socket.id);
     socket.join(room);
     console.log(sockets);
-    socket.emit('CONNECT', {sockets,currentChain});
+    socket.emit('CONNECT', {sockets});
     io.to(room).emit('hi');
 
-    socket.on('VOTE', (newVote) => {
-      console.log(newVote)
-      io.to(room).emit('NEW VOTE',newVote);
-    });
 
-    socket.on('SYNC', data=>{
-      io.to(room).emit('SYNC', data);
+    socket.on('CL REQUEST CHAIN',()=>{
+      const r = getRandomInt(0,blockchain.getLength());
+      socket.broadcast.to(sockets[r]).emit('SV REQUEST CHAIN',idSocket);
+    })
+    socket.on('CL SEND CHAIN', (idSocket,blockchain)=>{
+      socket.broadcast.to(idSocket).emit('SV SEND CHAIN',blockchain);
     })
 
-    socket.on('UPDATE', chain =>{
-      if(chain.length > currentChain.length) currentChain = chain;
+
+    socket.on('NEW BLOCK',() => {
+      const block = BlockChain.getLastedBlock();
+      io.to(room).emit('NEW BLOCK',block);
+    });
+
+    socket.on('COMPARE LATEST BLOCK', block =>{
+      console.log(block);
+      console.log('compare');
+      io.to(room).emit('COMPARE LATEST BLOCK',{idSocket:socket.id,block});
+    })
+
+    socket.on('RESULT COMPARE LATEST BLOCK', (idSocket,result) =>{
+      console.log('RESULT COMPARE LATEST BLOCK');
+      io.to(idSocket).emit('RESULT COMPARE LATEST BLOCK',{idSocket,result});
+    })
+
+    socket.on('COMPARE CHAIN', chain =>{
+      io.to(room).emit('COMPARE CHAIN',{idSocket:socket.id,chain});
+    })
+
+    socket.on('RESULT COMPARE CHAIN', (idSocket,blockchain,result) =>{
+      socket.broadcast.to(idSocket).emit('RESULT COMPARE CHAIN',{idSocket,blockchain,result});
     })
 
     socket.on('disconnect', () => {
-      sockets = sockets.filter(s => s===socket.id);
+      console.log('id socket leave ' + socket.id);
+      sockets = sockets.filter(s => s!=socket.id);
       socket.leave(room);
       console.log(sockets);
+      
     });
   });
 };
-
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 module.exports = socketListeners;
